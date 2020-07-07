8a1538a0903ef0def911c91d230aa7195e7751e3
rohan2734 <jagandevaki1@gmail.com>
2020-07-07 19:17:21 +0530

    created profile

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const bodyParser =require('body-parser');
 const app = express();
 
 const academicsRoutes = require('./routes/academics-routes');
-
-
+const profileRoutes = require('./routes/profile-routes');
 
 app.use('/api/faculty/academics',academicsRoutes);
+app.use('/api/faculty/prfile',profileRoutes);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Server initiated!");
