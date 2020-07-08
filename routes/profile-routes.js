d05b0e29a3dfbf5f47f22364535ea18c7a2b3045
rohan2734 <jagandevaki1@gmail.com>
2020-07-08 12:15:59 +0530

    added file uploads

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,11 +1,25 @@
 const express= require('express');
+const {check}= require('express-validator');
 
 const profileControllers = require('../controllers/profile-controllers');
-
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
-router.post('/',profileControllers.createProfile);
+router.post('/',
+fileUpload.single('image'),
+  [
+    check('fullName').not().isEmpty(),
+    check('address').not().isEmpty(),
+    check('registration').not().isEmpty(),
+    check('phoneNumber').isLength({min:10}),
+    check('bloodGroup').not().isEmpty(),
+    check('campusCode').not().isEmpty(),
+    check('emailId').normalizeEmail().isEmail(),
+    check('college').not().isEmpty(),
+    check('experience').not().isEmpty(),
+    check('duration').not().isEmpty()
+  ],profileControllers.createProfile);
 
 router.patch('/:profid',profileControllers.updateProfile);
 
