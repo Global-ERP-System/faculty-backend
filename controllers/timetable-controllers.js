38123699b5ea061fc87c6a451c3663b2b77da83a
rohan2734 <jagandevaki1@gmail.com>
2020-07-09 18:16:14 +0530

    minor changes

--- controllers/timetable-controllers.js
+++ controllers/timetable-controllers.js
@@ -2,19 +2,17 @@ const HttpError = require('../models/http-error'),
 Timetable = require('../models/timetableSchema');
 
 const getTimetableById = async (req,res,next)=>{
-    const timetableId = req.body.ttid;
-    let timetable;
-    try {
+     const timetableId = req.params.ttid;
+     let timetable;
+     try {
          timetable = await Timetable.find(t=> t.date === timetableId);
-    } catch (err) {
-       const  error = new HttpError(
-            'getting timtable failed,please try again',
-            500
+     } catch (err) {
+          const  error = new HttpError(
+               'getting timtable failed,please try again',500
           );
           return next(error);
-    }
+     }
     res.status(200).json({timetable});
-
 }
 
 module.exports = getTimetableById;
